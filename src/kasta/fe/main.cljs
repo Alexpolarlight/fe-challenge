aca8909eaa6cdd86aaa4b98602522c5854559486
Aleksandr <alexandr.kulakov@idapgroup.com>
2020-07-19 19:48:53 +0300

    Table done, data transformed into map, data display in progress.

--- src/kasta/fe/main.cljs
+++ src/kasta/fe/main.cljs
@@ -1,22 +1,69 @@
 (ns kasta.fe.main
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            [clojure.string :as string]
+            [kasta.shares.shares_mock :as shares]))
 
 (enable-console-print!)
-(println "Main println")
 
+(println "main  ")
+;(println "main Json " shares/parsedJson)
+(def sharesData shares/parsedJson)
+(def shares-state (atom sharesData
+;                          :id 121347
+;                          :name "H&M"
+;                          :description "Трендовые обновки для вашего гардероба "
+;                          :tags "FM"
+;                          :starts_at "2020-07-20T03:00:00.000Z"
+;                          :finishes_at "2020-07-21T03:00:00.000Z"
+
+))
+
+;;; Start Table
+;(defn filter-shares
+;  [filterstring]
+;  (filter #(re-find (->>(str filterstring)
+;                        (string/upper-case)
+;                        (re-pattern))
+;            (string/upper-case (:tags %)))
+;          @shares-state)
+;  )
+
+(rum/defc shares-table
+  [data]
+    (println "data " data)
+  [:table {:class "table"}
+   [:thead
+    [:tr
+     [:th "Name"]
+     [:th "Description"]
+     [:th "Tag"]
+     [:th "Starts at"]
+     [:th "Finishes at"]]]
+   [:tbody
+    ;     (for [{:keys [ id
+    ;                  name
+    ;                  description
+    ;                  tags
+    ;                  starts_at
+    ;                  finishes_at
+    ;                  ]}]
+    ;       ^{:key id}
+    [:tr
+     [:td "name"]
+     [:td "description"]
+     [:td "tags"]
+     [:td "starts_at"]
+     [:td "finishes_at"]
+     ]]])
+;; End Table
+
+;; Start Header
 (def state (atom "active"))
-;(def shares-state (atom shares))
-
-;(add-watch app-state :watcher)
-
 
 (rum/defc heading < rum.static [size text]
-  (do
-    (println text))
   [size text])
 
 (rum/defc tagSelector [tag]
-    (println 'tag' tag)
   [:div
    (heading :h3 "Active shares list")
    (heading :h4 "Filter tags list")
@@ -26,38 +73,25 @@
    [:button {:on-click #(reset! tag "C")} "Kids"]
    [:button {:on-click #(reset! tag "H")} "Home"]
    [:button {:on-click #(reset! tag "A")} "FoodVine"]
+   [:div (shares-table shares-state)]
    ]
 )
+;; End Header
 
-(defn shares-table
-  [filter]
-  [:table {:class "table table-condensed"}]
-    [:thead
-     [:tr
-      [:th "Name"]
-      [:th "Description"]
-      [:th "Tag"]
-      [:th "Starts at"]
-      [:th "Finishes at"]]]
-    [:tbody
-;     (for [{:keys [ name
-;                  description
-;                  tags
-;                  starts_at
-;                  finishes_at
-;                  ]}])
-     ]
-  )
 
+
+;; Start Render
 (add-watch state :render
            #(rum/mount
              (tagSelector state)
            (js/document.getElementById "content")))
 
 (rum/defc Root []
-  (println "state " state)
-  [(tagSelector state)])
-
+;  (println "shares-state - " shares-state)
+;  (println "state - " state)
+  [(tagSelector state)]
+  )
 
 (defn ^:export trigger-render []
   (rum/mount (Root) (js/document.getElementById "content")))
+;; End Render
